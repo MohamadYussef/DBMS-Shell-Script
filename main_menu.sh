ab913dd8bfd480e1000dbfb63afb7e456b704cd2
Moe <Muhamad.Qubaisi@gmail.com>
2023-04-01 08:31:25 +0200

    testing something

--- main_menu.sh
+++ main_menu.sh
@@ -2,19 +2,18 @@
 
 main_menu() {
     PS3="choose an option in the Database Engine: "
-    select choise in "Create Database" "List Databases" "Connect To Databases" "Drop Database" "exit"
-    do
+    select choise in "Create Database" "List Databases" "Connect To Databases" "Drop Database" "exit"; do
         case $choise in
-            "Create Database") create_DB;;
-            "List Databases") ls -lF ./DB | grep /;;
-            "Connect To Databases") echo co;;
-            "Drop Database") drop_DB;;
-            "exit") exit;;
-            *)echo "Invalid choise";;
+        "Create Database") create_DB ;;
+        "List Databases") ls -lF ./DB | grep / ;;
+        "Connect To Databases") echo co ;;
+        "Drop Database") drop_DB ;;
+        "exit") exit ;;
+        *) echo "Invalid choise" ;;
         esac
-done    
+    done
 }
-create_DB(){
+create_DB() {
     read -p "Enter the name of the Database that will be created: " DB_name
     if [ -e ./DB/"$DB_name" ]; then
         echo "sorry that Database name exist"
@@ -29,10 +28,10 @@ create_DB(){
     fi
 }
 
-drop_DB(){
+drop_DB() {
     ls -F ./DB/ | grep /
     read -p "Enter the Database you want to remove: " DB_drop
-    if [[ $(find ./DB -name $DB_drop) ]] ;then
+    if [[ $(find ./DB -name $DB_drop) ]]; then
         rm -r ./DB/$DB_drop
         echo "The $DB_drop is removed successfully"
     else
@@ -41,3 +40,4 @@ drop_DB(){
 }
 
 main_menu
+echo test
