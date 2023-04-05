65b65647b43dbaebf314760d4342aec2db625bc4
MoeQubaisi <Muhamad.Qubaisi@gmail.com>
2023-04-05 19:01:56 +0200

    added update_function

--- main_menu.sh
+++ main_menu.sh
@@ -92,15 +92,15 @@ connect_db() {
 
 db_actions() {
     echo "Choose an action: "
-    select db_action in "Create table" "Drop table" "Insert table" "Select table" "Delete Table" "List table" "Update table" "Return to the main menu"; do
+    select db_action in "Create Table" "Drop Table" "Insert Table" "Select Table" "Delete Table" "List Table" "Update Table" "Return to the main menu"; do
         case $db_action in
-        "Create table") create_table ;;
-        "Drop table") echo "function not added yet" ;;
-        "Insert table") insert_table ;;
-        "Select table") select_table ;;
+        "Create Table") create_table ;;
+        "Drop Table") echo "function not added yet" ;;
+        "Insert Table") insert_table ;;
+        "Select Table") select_table ;;
         "Delete Table") delete_table ;;
         "List Table") echo "function not added yet" ;;
-        "Update Table") echo "function not added yet" ;;
+        "Update Table") update_table ;;
         "Return to the main menu")
             cd ../..
             clear
@@ -112,27 +112,27 @@ db_actions() {
 }
 
 create_table() {
-    read -p "Enter the table name: " t_name
-    if [ -e ./$t_name ]; then
+    read -p "Enter the table name: " table_name
+    if [ -e ./$table_name ]; then
         echo "Table already exist"
         create_table
     else
         if [[ "$db_name" =~ ^[a-zA-Z]+[a-zA-Z1-9_]+ ]]; then
-            touch ./$t_name
+            touch ./$table_name
             read -p "Enter the number of fields: " fields_num
             read -p "Enter Primary key: " pk
-            echo -n ${pk}: >>$t_name
+            echo -n ${pk}: >>$table_name
             index=1
             while [ $index -lt $fields_num ]; do
                 read -p "Enter name for field $index: " field_name
-                echo -n ${field_name}: >>$t_name
+                echo -n ${field_name}: >>$table_name
                 let index++
             done
             index=1
             while true; do
                 read -p "Enter Primary key data type: " pk_t
                 if [ $pk_t = int ] || [ $pk_t = string ]; then
-                    echo -en "\n${pk_t}": >>$t_name
+                    echo -en "\n${pk_t}": >>$table_name
                     break
                 else
                     echo "Invalid datatype"
@@ -140,38 +140,38 @@ create_table() {
             done
             while [ $index -lt $fields_num ]; do
                 read -p "Enter data type for field $index: " field_type
-                if [ $field_type = int ] || [ $field_type = string ] || [ $field_type = bool ];
-                then 
-                    echo -n ${field_type}: >>$t_name
+                if [ $field_type = int ] || [ $field_type = string ] || [ $field_type = bool ]; then
+                    echo -n ${field_type}: >>$table_name
                     let index++
                 else
                     echo "Invalid datatype"
                 fi
             done
+            echo "" >>$table_name
         else
             echo "Invalid name"
             create_table
         fi
-        sed -i 's/:$//' $t_name &
+        sed -i 's/:$//' $table_name &
     fi
 }
 
-check_datatype(){
+check_datatype() {
     datatype_postion=$1
-    data_type=`awk -F: -v var="$datatype_postion" '{if(NR==2)print $var}' $ins_table`
-    if [ "$data_type" = int ];then
+    data_type=$(awk -F: -v var="$datatype_postion" '{if(NR==2)print $var}' $ins_table)
+    if [ "$data_type" = int ]; then
         if ! [[ "$field_data" =~ ^[0-9]+$ ]]; then
             echo "Invalid input by data type"
         else
             status=done
         fi
-    elif [ "$data_type" = string ];then
+    elif [ "$data_type" = string ]; then
         if ! [[ "$field_data" =~ ^[a-zA-Z]+[a-zA-Z1-9_]+ ]]; then
             echo "Invalid input by data type"
         else
             status=done
         fi
-    elif [ "$data_type" = bool ];then
+    elif [ "$data_type" = bool ]; then
         if ! [[ "$field_data" =~ [tT]rue ]] || ! [[ "$field_data" =~ [fF]alse ]]; then
             echo "Invalid input by data type"
         else
@@ -180,22 +180,21 @@ check_datatype(){
     fi
 }
 
-insert_table (){
+insert_table() {
     clear
     ls
     read -p "Enter the table that you want to insert it: " ins_table
-    if [[ `find -name $ins_table 2> /dev/null` ]];then
-        fields_num=`awk -F: '{print NF}' $ins_table | head -1`
-        for ((i=1; i<=$fields_num;i++));do
+    if [[ $(find -name $ins_table 2>/dev/null) ]]; then
+        fields_num=$(awk -F: '{print NF}' $ins_table | head -1)
+        for ((i = 1; i <= $fields_num; i++)); do
             status=""
             if [ $i -eq 1 ]; then
                 while true; do
-                    if [ "$status" = "done" ];then
+                    if [ "$status" = "done" ]; then
                         break
                     fi
                     read -p "enter data in field number $i: " field_data
-                    if [[ `awk -F: '{if(NR>2)print $1}' $ins_table | grep $field_data 2> /dev/null` ]]
-                    then
+                    if [[ $(awk -F: '{if(NR>2)print $1}' $ins_table | grep $field_data 2>/dev/null) ]]; then
                         echo "id is duplicated"
                     elif [ -z $field_data ]; then
                         echo "id is NULL"
@@ -206,7 +205,7 @@ insert_table (){
             else
                 status=""
                 while true; do
-                    if [ "$status" = "done" ];then
+                    if [ "$status" = "done" ]; then
                         break
                     fi
                     read -p "enter data in field number $i: " field_data
@@ -216,10 +215,10 @@ insert_table (){
                 done
             fi
             data=$data:${field_data}
-            
+
             # echo -n "${field_data}:" >>$ins_table
         done
-        echo $data:>>$ins_table
+        echo $data: >>$ins_table
         data=""
         sed -i 's/:$//' $ins_table
         sed -i 's/^://' $ins_table
@@ -232,39 +231,39 @@ insert_table (){
     fi
 }
 
-select_table(){
+select_table() {
     clear
-    select_col(){
-        select col in `head -1 $select_table | tr ":" " "`;do
-            case $REPLY in 
-                $REPLY) 
-                    if (($REPLY > 0 && $REPLY <= `head -1 m |tr ":" " "|wc -w`)); then
-                        awk -F: -v var=$REPLY '{if(NR>2){print $var}}' $select_table
-                    else
-                        echo "col not exist"
-                    fi
-                    ;;
-                *)echo "col not exist";;
+    select_col() {
+        select col in $(head -1 $select_table | tr ":" " "); do
+            case $REPLY in
+            $REPLY)
+                if (($REPLY > 0 && $REPLY <= $(head -1 m | tr ":" " " | wc -w))); then
+                    awk -F: -v var=$REPLY '{if(NR>2){print $var}}' $select_table
+                else
+                    echo "col not exist"
+                fi
+                ;;
+            *) echo "col not exist" ;;
             esac
         done
     }
     ls
     read -p "Enter the table that you want to select from it: " select_table
-    if [[ `find -name $select_table 2> /dev/null` ]];then
-        select opt in "select by all" "select by col" "select by specific word" "exit";do
-            case $opt in 
-                "select by all") sed -n '3,$p' $select_table;;
-                "select by col") select_col;;
-                "select by specific word") 
-                    read -p "enter the word: " word
-                    if [[ `tail -n +3 $select_table |grep -i "$word"` ]]; then
-                        echo `tail -n +3 $select_table |grep -i "$word"`
-                    else
-                        echo "$word doesn't exsit"
-                    fi
+    if [[ $(find -name $select_table 2>/dev/null) ]]; then
+        select opt in "select by all" "select by col" "select by specific word" "exit"; do
+            case $opt in
+            "select by all") sed -n '3,$p' $select_table ;;
+            "select by col") select_col ;;
+            "select by specific word")
+                read -p "enter the word: " word
+                if [[ $(tail -n +3 $select_table | grep -i "$word") ]]; then
+                    echo $(tail -n +3 $select_table | grep -i "$word")
+                else
+                    echo "$word doesn't exsit"
+                fi
                 ;;
-                "exit") db_actions;;
-                *) echo invalid choise
+            "exit") db_actions ;;
+            *) echo invalid choise ;;
             esac
         done
     else
@@ -276,12 +275,12 @@ select_table(){
     fi
 }
 
-delete_table(){
+delete_table() {
     ls
     read -p "Enter the table that you want to delete from it: " select_table
-    if [[ `find -name $select_table 2> /dev/null` ]];then
+    if [[ $(find -name $select_table 2>/dev/null) ]]; then
         read -p "enter the word from the row you want to delete: " word
-        if [[ `tail -n +3 $select_table|grep -i "$word"` ]]; then
+        if [[ $(tail -n +3 $select_table | grep -i "$word") ]]; then
             sed -i "/$word/d" $select_table
             echo "the row deleted successfully "
         else
@@ -289,6 +288,32 @@ delete_table(){
         fi
     else
         echo "$select_table doesn't exist"
+    fi
+}
+
+update_table() {
+    read -p "Enter Table Name : " table_name
+    if [ $(ls | grep -x $table_name) ]; then
+        read -p "Enter Field Name : " field_name
+        fields=($(sed -n '1p' $table_name | sed -n 's/:/ /gp'))
+        match_found=""
+        for field in ${fields[@]}; do
+            if [ $field_name = $field ]; then
+                match_found=true
+            fi
+        done
+        if [ $match_found ]; then
+            read -p "Enter Primary key" pk
+            if [ $(cut -d : -f 1 $table_name | sed -n '3,$p' | grep $pk) ]; then
+                echo "Primary key found"
+            else
+                echo "Primary key not found"
+            fi
+        else
+            echo "Field Doesn't Exist"
+        fi
+    else
+        echo "Table Doesn't Exist"
     fi
 }
 
